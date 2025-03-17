72519e0f4f495fe8ac1629c1711d1e1d72a0c36d
Sirawit Longjun <sirawit9874@gmail.com>
2025-03-17 10:33:42 +0700

    add my demo project

---
+++ tripinfo.js
@@ -0,0 +1,31 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+    
+    var input = document.getElementById('location');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+}
+
+// Function to capture user input when the "Next" button is clicked
+document.querySelector(".next-btn").addEventListener("click", function() {
+    // Capture input values
+    var tripName = document.getElementById("trip-name").value;
+    var location = document.getElementById("location").value;
+    var budget = document.getElementById("budget").value;
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+
+    // You can store this data or pass it to another page if needed.
+    // For example, if you want to pass this data to the next page:
+    localStorage.setItem("tripName", tripName);
+    localStorage.setItem("location", location);
+    localStorage.setItem("budget", budget);
+    localStorage.setItem("from", from);
+    localStorage.setItem("to", to);
+
+    // Navigate to the next page
+    window.location.href = 'createTrip2.html';
+});
